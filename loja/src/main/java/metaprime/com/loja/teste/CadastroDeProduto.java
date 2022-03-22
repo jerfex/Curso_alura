ae97cae8898657b1ea53a04cc57718a9eb4d026e
Jerfeson Campos <jerfncs@gmail.com>
2022-03-22 12:19:52 -0300

    Curso_alura_loja
    Criação de uma jpa de uma loja com hibernat

---
+++ loja/src/main/java/metaprime/com/loja/teste/CadastroDeProduto.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metaprime.com.loja.teste;
+
+import java.math.BigDecimal;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import metaprime.com.lojas.Produto;
+
+/**
+ *
+ * @author metaprime
+ */
+public class CadastroDeProduto {
+    
+    public static void main(String[] args) {
+        Produto celularProduto = new Produto();
+        celularProduto.setNome("Xiaomi Redmi");
+        celularProduto.setDescricao("Muito popular no Brasil");
+        celularProduto.setPreco(BigDecimal.valueOf(800));
+        
+        EntityManagerFactory factory  = Persistence.createEntityManagerFactory("ProdPU");
+        EntityManager em = factory.createEntityManager();
+        
+        em.persist(celularProduto);
+        
+    }
+    
+}
---
+++ loja/src/main/java/metaprime/com/lojas/Produto.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metaprime.com.lojas;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author metaprime
+ */
+@Entity  //para indicar que é uma entidade da jpa
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+}
